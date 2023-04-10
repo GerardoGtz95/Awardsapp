5737f8c949210c1b57903d40c5b5a68f0cc58446
Gerardo Gutierrez <gerardo.gutierrez@caliente.mx>
2023-04-10 14:11:06 -0600

    Models setup

--- awardsapp/polls/models.py
+++ awardsapp/polls/models.py
@@ -1,12 +1,24 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateField("date published")
+    pub_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question_text
+
+        def was_published_recently(self):
+            return self.pub_date >= timezone.now - datetime.timedelta(days=1)
 
 
-class Choices(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+class Choice(models.Model):
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name='question_choices')
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
